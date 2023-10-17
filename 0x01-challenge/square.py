2e13aba6d8eb139a3aeceb08d1682e54a747ef5f
magedsaif <magedsaif21@gmail.com>
2023-10-17 12:14:36 +0200

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
 
     def area_of_my_square(self):
         """Area of the square."""
-        return self.width * self.height
+        return self.width * self.width
 
     def Permiter_Of_MySquare(self):
         """Perimeter of the square."""
