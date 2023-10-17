d542c977f9f97849ff05f05bb1087bf15dd65824
magedsaif <magedsaif21@gmail.com>
2023-10-17 12:15:30 +0200

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,7 +10,6 @@ class Square():
 
     def __init__(self, *args, **kwargs):
         """Initialize Square class."""
-        self.width = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,7 +17,7 @@ class Square():
         """Area of the square."""
         return self.width * self.width
 
-    def Permiter_Of_MySquare(self):
+    def Permiter_Of_My_Square(self):
         """Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
@@ -32,4 +31,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.Permiter_Of_My_Square())
